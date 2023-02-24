e143dd6085fb38ff37047d20e3004bfb83129cdf
Diego <diewentdiaz@gmail.com>
2023-02-24 17:29:13 +0100

    Internazionalization in English

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ from kivy.config import Config
 from kivy.resources import resource_add_path
 
 import app_config
+import i18n
 from ui.main import Main
 
 if __name__ == '__main__':
@@ -13,5 +14,10 @@ if __name__ == '__main__':
     if hasattr(sys, '_MEIPASS'):
         resource_add_path(os.path.join(sys._MEIPASS))
 
+    # Load internazionalization
+    i18n.load_path.append(app_config.BASE_PATH + '/ui/i18n')
+    i18n.set('locale', app_config.LANGUAGE)
+
+    # Load main app
     main_app = Main()
     main_app.run()
